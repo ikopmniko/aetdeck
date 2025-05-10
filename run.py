796a0f1ccc902b3567043d5af5706e891a535385
ikopmniko <hannapitman786@gmail.com>
2025-05-10 13:38:26 +0700

    Update run.py

--- run.py
+++ run.py
@@ -14,11 +14,11 @@ SUPABASE_TABLE_NAME = "sp"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Rentang data yang diproses (misal dari baris 1 sampai 50)
-start_row =800  # Baris pertama (0-based index)
-end_row = 900  # Baris terakhir yang ingin diproses
+start_row =900  # Baris pertama (0-based index)
+end_row = 1000  # Baris terakhir yang ingin diproses
 
 # Deklarasi akun tunggal
-email = "iman_spring@sdn2duwet.ac.id"
+email = "marianne_reed@sdn2duwet.ac.id"
 password = "@@Masuk123#"
 
 

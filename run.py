5299883b6e402ea147c9f0fa41c4fdafa23d9b31
ikopmniko <hannapitman786@gmail.com>
2025-05-10 13:24:19 +0700

    Update run.py

--- run.py
+++ run.py
@@ -13,17 +13,13 @@ SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ
 SUPABASE_TABLE_NAME = "sp"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-
 # Rentang data yang diproses (misal dari baris 1 sampai 50)
-start_row =950  # Baris pertama (0-based index)
-end_row = 1000 # Baris terakhir yang ingin diproses
+start_row =0  # Baris pertama (0-based index)
+end_row = 100  # Baris terakhir yang ingin diproses
 
 # Deklarasi akun tunggal
-email = "amalia_tolliver@sdn2duwet.ac.id"
+email = "brendon_ferrer@sdn2duwet.ac.id"
 password = "@@Masuk123#"
-
-
-
 
 
 def random_string(count):
@@ -97,55 +93,57 @@ titles = read_csv_range("data.csv", start_row, end_row)
 
 for title in titles:
     
-
-    judul = title
-    modif_kata = title.replace(' ', '_')
-    kw = f'{title} Leaked Video New Upload'
-
-    konten = f'''
-    29 minutes ago - Access {title} Leaked Only-fans New Updaload FIles 2025<br><br>LINK ⏩⏩ https://topallfans.web.app?title={modif_kata}<br><br>{title} Leaked only-fans update pict  - you ll be able to download and preview all content from {title} Only-fans in just a few clicks.
-    '''
-
-        
-
-    driver.get("https://aetherhub.com/Deck/MyDecks/")
-    time.sleep(3)
-    driver.find_element(By.CSS_SELECTOR, "#NewDeck_Name").send_keys(kw)
-    time.sleep(2)
-        
-
-    driver.find_element(By.XPATH, "//button[.//b[text()='Create']]").click()
-    time.sleep(5)
-
     try:
-        driver.execute_script("document.getElementById('zone16-DesktopSticky').style.display = 'none';")
+        judul = title
+        modif_kata = title.replace(' ', '_')
+        kw = f'{title} Leaked Video New Files Update ++ '
+
+        konten = f'''
+        29 minutes ago - Access {title} Leaked Only-fans New Updaload FIles 2025<br><br>LINK ⏩⏩ https://topallfans.web.app?title={modif_kata}<br><br>{title} Leaked only-fans update pict  - you ll be able to download and preview all content from {title} Only-fans in just a few clicks.
+        '''
+
+            
+
+        driver.get("https://aetherhub.com/Deck/MyDecks/")
+        time.sleep(3)
+        driver.find_element(By.CSS_SELECTOR, "#NewDeck_Name").send_keys(kw)
+        time.sleep(2)
+            
+
+        driver.find_element(By.XPATH, "//button[.//b[text()='Create']]").click()
+        time.sleep(5)
+
+        try:
+            driver.execute_script("document.getElementById('zone16-DesktopSticky').style.display = 'none';")
+        except:
+            pass
+
+        element = driver.find_element(By.CSS_SELECTOR, "#editNotes")
+
+        # Scroll 
+        driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
+        time.sleep(2)
+
+        driver.find_element(By.CSS_SELECTOR, "#editNotes").click()
+        time.sleep(3)
+
+        driver.execute_script("tinymce.activeEditor.setContent(arguments[0]);", f'{judul} Leaked Video 2025 <br><br> LINK ⏩⏩ <a href="https://clipsfans.com/{modif_kata}&ref=aet">https://clipsfans.com/{modif_kata}</a> ')
+
+            
+        driver.find_element(By.CSS_SELECTOR, "#notesSubmit").click()
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//a[@title='Visual View']").click()
+        time.sleep(3)
+
+        response = (
+                supabase.table(SUPABASE_TABLE_NAME)
+                .insert({"result": driver.current_url})
+                .execute()
+            )
+
+        print("Berhasil Upload : "+title)
     except:
-        pass
-
-    element = driver.find_element(By.CSS_SELECTOR, "#editNotes")
-
-    # Scroll 
-    driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
-    time.sleep(2)
-
-    driver.find_element(By.CSS_SELECTOR, "#editNotes").click()
-    time.sleep(3)
-
-    driver.execute_script("tinymce.activeEditor.setContent(arguments[0]);", f'{judul} Leaked Video 2025 <br><br> LINK ⏩⏩ <a href="https://clipsfans.com/{modif_kata}&ref=aet">https://clipsfans.com/{modif_kata}</a> ')
-
-        
-    driver.find_element(By.CSS_SELECTOR, "#notesSubmit").click()
-    time.sleep(5)
-    driver.find_element(By.XPATH, "//a[@title='Visual View']").click()
-    time.sleep(3)
-
-    response = (
-            supabase.table(SUPABASE_TABLE_NAME)
-            .insert({"result": driver.current_url})
-            .execute()
-        )
-
-    print("Berhasil Upload : "+title)
+        print("Terjadi Error")
 
     
 

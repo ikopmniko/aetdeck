2b98578507b0ad3158d26225cd46d59d6c9dfeb6
ikopmniko <hannapitman786@gmail.com>
2025-05-12 00:24:15 +0700

    Update run.py

--- run.py
+++ run.py
@@ -14,11 +14,11 @@ SUPABASE_TABLE_NAME = "aeteronlyy"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Rentang data yang diproses (misal dari baris 1 sampai 50)
-start_row =2900  # Baris pertama (0-based index)
-end_row = 3000  # Baris terakhir yang ingin diproses
+start_row =3000  # Baris pertama (0-based index)
+end_row = 3100  # Baris terakhir yang ingin diproses
 
 # Deklarasi akun tunggal
-email = "johan_bustos@sdn2duwet.ac.id"
+email = "josef_carmona@sdn2duwet.ac.id"
 password = "@@Masuk123#"
 
 

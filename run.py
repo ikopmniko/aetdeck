838b9b2d964025ace0dcc78f54ba653e03cba0b4
ikopmniko <hannapitman786@gmail.com>
2025-05-12 01:09:18 +0700

    Update run.py

--- run.py
+++ run.py
@@ -14,10 +14,10 @@ SUPABASE_TABLE_NAME = "aeteronlyy"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Rentang data yang diproses (misal dari baris 1 sampai 50)
-start_row = 4900
-end_row = 5000
+start_row = 5000
+end_row = 5100
 
-email = "gerald_cheng@sdn2duwet.ac.id"
+email = "adelaide_mcwilliams@sdn2duwet.ac.id"
 password = "@@Masuk123#"
 
 
@@ -69,7 +69,7 @@ time.sleep(3)
 driver.find_element(By.CSS_SELECTOR, "#Google").click()
 time.sleep(3)
 
-clickgoogle2 = f'div[data-identifier="gerald_cheng@sdn2duwet.ac.id"]'
+clickgoogle2 = f'div[data-identifier="adelaide_mcwilliams@sdn2duwet.ac.id"]'
 
 driver.find_element(By.CSS_SELECTOR, clickgoogle2).click()
 time.sleep(3)
